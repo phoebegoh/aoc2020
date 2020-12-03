ab155719ddff875b755421f46991bb703a1e6863
Phoebe Goh <phoebegoh@gmail.com>
2020-12-03 01:39:24 -0500

    Make day 3 neater

--- day3/day3.py
+++ day3/day3.py
@@ -3,81 +3,30 @@
 # how many trees would you encounter?
 
 
-track = 1
-
 with open('input') as f:
     treemap = [line.split() for line in f]
 
-trees = 0
-i = 0
-for rows in treemap:
-    for row in rows:
-        print("row: {}, col: {} is {}".format(row,i,row[i]))
-        if(row[i] == "#"):
-            trees += 1
-        i += 3
-        if (i > len(row)-1):
-            i -= len(row)
 
-track *= trees
-print("trees: {}, total: {}".format(trees,track))
-
-trees = 0
-i = 0
-for rows in treemap:
-    for row in rows:
-        #print("row: {}, col: {} is {}".format(row,i,row[i]))
-        if(row[i] == "#"):
-            trees += 1
-        i += 1
-        if (i > len(row)-1):
-            i -= len(row)
-
-track *= trees
-print("trees: {}, total: {}".format(trees,track))
-
-trees = 0
-i = 0
-for rows in treemap:
-    for row in rows:
-        #print("row: {}, col: {} is {}".format(row,i,row[i]))
-        if(row[i] == "#"):
-            trees += 1
-        i += 5
-        if (i > len(row)-1):
-            i -= len(row)
-
-track *= trees
-print("trees: {}, total: {}".format(trees,track))
-
-trees = 0
-i = 0
-for rows in treemap:
-    for row in rows:
-        #print("row: {}, col: {} is {}".format(row,i,row[i]))
-        if(row[i] == "#"):
-            trees += 1
-        i += 7
-        if (i > len(row)-1):
-            i -= len(row)
-
-track *= trees
-print("trees: {}, total: {}".format(trees,track))
-
-trees = 0
-i = 0
-countrow = 0
-for rows in treemap:
-    if (countrow % 2 == 0):
-        for row in rows:
-            #print("checking row: "+ str(countrow))
-            print("row: {}, col: {} is {}".format(row,i,row[i]))
-            if(row[i] == "#"):
-                trees += 1
-            i += 1
+def count_trees(inc_across,inc_down):
+    trees = 0
+    i = 0
+    rowcount = 0
+    for rows in treemap:
+        if (rowcount % inc_down == 0):
+            for row in rows:
+                #print("row: {}, col: {} is {}".format(row,i,row[i]))
+                if(row[i] == "#"):
+                    trees += 1
+                i += inc_across
             if (i > len(row)-1):
                 i -= len(row)
-    countrow += 1
+        rowcount += 1
+    return trees
 
-track *= trees
-print("trees: {}, total: {}".format(trees,track))
+track = count_trees(1,1) * count_trees(3,1) * count_trees(5,1) * count_trees(7,1) * count_trees(7,2)
+print(track)
+print(count_trees(1,1))
+print(count_trees(3,1))
+print(count_trees(5,1))
+print(count_trees(7,1))
+print(count_trees(7,2))
