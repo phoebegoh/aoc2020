63e4e85a3a92e944394e811fd56c45488dc656c4
Phoebe Goh <phoebegoh@gmail.com>
2020-12-06 12:28:10 -0500

    add comment after i woke up

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,4 @@
-# Create a set of all possible answers
+# Create a list of all possible answers
 # Create a running total
 questions = "abcdefghijklmnopqrstuvwxyz"
 total = 0
@@ -17,11 +17,12 @@ for group in groups:
         if c not in answers:
             answers += c
 
+    # count is the length of the list of total answers from this group
     count = len(set(questions) & set(answers))
     #print("group:",chars)
     #print("answers:",answers)
     #print("count:",count)
-    #print("---")
+    # print("---")
     total += count
 
 print("part 1 answer:",total)
@@ -30,11 +31,16 @@ total = 0
 for group in groups:
     x = 0
     answers = set()
+
     people = group.splitlines()
-    print("answers:",people)
-    result = set(set(people[0]).intersection(*people))
-    print("intersect:",result)
-    print("---")
+    result = set(set(people[0]).intersection(*people)) 
+    # I learned something new. This is the intersection of ALL the lists in people.
+    # https://www.semicolonworld.com/question/53496/python-intersection-of-multiple-lists
+
+
+    #print("answers:",people)
+    #print("intersect:",result)
+    #print("---")
     total += len(result)
 
 print("part 2 answer:",total)
