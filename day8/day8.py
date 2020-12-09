fd778ef3896e474509551985d7f3020f8ba71d46
Phoebe Goh <phoebegoh@gmail.com>
2020-12-09 00:47:39 -0500

    day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -6,19 +6,8 @@ with open('input','r') as f:
 instructions = []
 instructions = [line.split() for line in data]
 completed = [0 for x in range(len(instructions))]
-
-# 
-def do_wop(arr):
-    action = arr[0]
-    ctr = arr[1]
-    print("Running",action,ctr)
-    if action == "nop":
-        index += 1
-    if action == "jmp":
-        index += ctr
-    if action == "acc":
-        acc += ctr
-        index += ctr
+start = 0
+acc = 0
 
 # Return True if the last command in a program was run
 def is_complete():
@@ -27,32 +16,61 @@ def is_complete():
     return False
 
 # Return True if this command was already run
-def already_run(index):
+def is_infinite(index):
     if completed[index] == 1:
         return True
     return False
 
-index = 0
-acc = 0
-while not is_complete():
+# Iterate over the instructions array starting at index
+# If you find a jmp, flip it to nop (and vice versa) and then return
+def flip_jmpnop(index):
 
-    if already_run(index):
-        print("Already run step at position",index)
-        break
+    for i in range(index,len(instructions)-1):
 
-    action = instructions[index][0]
-    ctr = int(instructions[index][1])
+        if instructions[i][0] == 'jmp':
+            #print("Flipped",i,"to nop")
+            instructions[i][0] = 'nop'
+            return
+        elif instructions[i][0] == 'nop':
+            #print("Flipped",i,"to jmp")
+            instructions[i][0] = 'jmp'
+            return
+        else:
+            pass
+    return
 
-    print("Running index",index,'-',action,ctr)
-    completed[index] = 1
-    if action == "nop":
-        index += 1
-    if action == "jmp":
-        index += ctr
-    if action == "acc":
-        acc += ctr
-        index += 1
+def run_program(index):
+    global acc
+
+    while not is_complete():
+
+        if is_infinite(index):
+            print("Breaking on infinite loop",index)
+            return False
+
+        action = instructions[index][0]
+        ctr = int(instructions[index][1])
+
+        #print("Running index",index,'-',action,ctr)
+        completed[index] = 1
+        if action == "nop":
+            index += 1
+        if action == "jmp":
+            index += ctr
+        if action == "acc":
+            acc += ctr
+            index += 1
+
+    return True
+
+checked = 0
+while not run_program(0):
+    acc = 0
+    flip_jmpnop(checked)
+    completed = [0 for x in range(len(instructions))]
+    run_program(0)
+    flip_jmpnop(checked)
+    checked += 1
     
-    print(" Acc:",acc)
-
-print("Part 1:",acc)
+#print("Part 1:",acc)
+print("Part 2",acc)
